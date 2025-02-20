e59828336e204c2773d0d96114e1c4a40ce5a121
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-02-19 23:43:55 -0500

    Comments added

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
+
+    // Plane object so we can follow it's position or have a reference
     public GameObject plane;
+    // The camera offset
     private Vector3 offset = new Vector3(22.5f, 0.68f, 12.3f);
 
     // Start is called before the first frame update
@@ -16,6 +19,7 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        // Adds offset to the planes original position which then becomes the camera position.
         transform.position = plane.transform.position + offset;
     }
 }
